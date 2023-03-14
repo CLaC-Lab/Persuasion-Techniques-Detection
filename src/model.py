5e3062a4c2d5f5006e388c2faf147c656a71504c
Bryce Hamilton <brhamilton@expediagroup.com>
2023-03-13 23:15:07 -0400

    refactor v1

---
+++ src/model.py
@@ -0,0 +1,78 @@
+
+import torch
+import transformers
+
+import pytorch_lightning as pl
+
+from torch.optim import AdamW
+
+MODEL = "roberta-base"
+
+class BERTTagger(pl.LightningModule):
+  def __init__(self, n_classes: int, n_training_steps=None, n_warmup_steps=None):
+    super().__init__()
+    self.bert = transformers.AutoModel.from_pretrained(MODEL, return_dict=True)
+    self.classifier = torch.nn.Linear(self.bert.config.hidden_size, n_classes)
+    self.n_training_steps = n_training_steps
+    self.n_warmup_steps = n_warmup_steps
+    self.criterion = torch.nn.BCELoss()
+
+  def forward(self, input_ids, attention_mask, labels=None):
+    output = self.bert(input_ids, attention_mask=attention_mask)
+    output = self.classifier(output.pooler_output)
+    output = torch.sigmoid(output)
+    loss = 0
+    if labels is not None:
+        loss = self.criterion(output, labels)
+    return loss, output
+
+  def training_step(self, batch):
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    labels = batch["labels"]
+    loss, outputs = self(input_ids, attention_mask, labels)
+    self.log("train_loss", loss, prog_bar=True, logger=True)
+    return {"loss": loss, "predictions": outputs, "labels": labels}
+
+  def validation_step(self, batch):
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    labels = batch["labels"]
+    loss, outputs = self(input_ids, attention_mask, labels)
+    self.log("val_loss", loss, prog_bar=True, logger=True)
+    return loss
+
+  def test_step(self, batch):
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    labels = batch["labels"]
+    loss, outputs = self(input_ids, attention_mask, labels)
+    self.log("test_loss", loss, prog_bar=True, logger=True)
+    return loss
+
+  def training_epoch_end(self, outputs):
+    labels = []
+    predictions = []
+    for output in outputs:
+      for out_labels in output["labels"].detach().cpu():
+        labels.append(out_labels)
+      for out_predictions in output["predictions"].detach().cpu():
+        predictions.append(out_predictions)
+    labels = torch.stack(labels).int()
+    predictions = torch.stack(predictions)
+
+
+  def configure_optimizers(self):
+    optimizer = AdamW(self.parameters(), lr=2e-5)
+    scheduler = transformers.get_linear_schedule_with_warmup(
+      optimizer,
+      num_warmup_steps=self.n_warmup_steps,
+      num_training_steps=self.n_training_steps
+    )
+    return dict(
+      optimizer=optimizer,
+      lr_scheduler=dict(
+        scheduler=scheduler,
+        interval='step'
+      )
+    )
