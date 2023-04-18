9a8c3c8443d7e1733c06bfb9f9a9405a8d99aaf7
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2023-04-18 13:35:39 -0400

    created data folder

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,8 +7,8 @@ from labels import LABEL_COLUMNS
 
 def read_process_write(split, lang):
 
-   labels_input_file = os.path.join("..", "semeval2023task3bundle-v4", "data", lang, f"{split}-labels-subtask-3.txt")
-   text_input_file = os.path.join("..", "semeval2023task3bundle-v4", "data", lang, f"{split}-labels-subtask-3.template")
+   labels_input_file = os.path.join('..', 'semeval2023task3bundle-v4', 'data', lang, f'{split}-labels-subtask-3.txt')
+   text_input_file = os.path.join('..', 'semeval2023task3bundle-v4', 'data', lang, f'{split}-labels-subtask-3.template')
 
    df_labels = pd.read_csv(labels_input_file, sep='\t',
                             header=None,
@@ -19,25 +19,28 @@ def read_process_write(split, lang):
                           names={'article': int, 'paragraph': int, 'text': str},
                           on_bad_lines='skip')
 
-   df_labels['labels'] = np.where(df_labels['labels'].isna(), "", df_labels['labels'])
+   df_labels['labels'] = np.where(df_labels['labels'].isna(), '', df_labels['labels'])
 
    df = df_text.merge(df_labels, how='inner', on=['article', 'paragraph'])
 
    for label in LABEL_COLUMNS:
       df[label] = np.where(df['labels'].str.contains(label), 1, 0)
-   df = df.drop(columns="labels")
+   df = df.drop(columns='labels')
 
-   output_file = os.path.join("..", "semeval2023task3bundle-v4", f"{split}_df_{lang}.csv")
+   output_file = os.path.join('..', 'data', f'{split}_df_{lang}.csv')
    df.to_csv(output_file, index=False)
 
 def main():
+
+   if not os.path.isdir('../data'):
+      os.makedirs('../data')
 
    LANGS = ['en', 'fr', 'ge', 'it', 'po', 'ru']
 
    for lang in LANGS:
       print(lang)
-      read_process_write("train", lang)
-      read_process_write("dev", lang)
+      read_process_write('train', lang)
+      read_process_write('dev', lang)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
    main()
